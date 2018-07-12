57d7b8f1f0f925f254b61e0d00954e08d2a6caea
freedomzzzii <theeraporn.tia@gmail.com>
2018-07-12 14:15:08 +0700

    [update] theme app

---
+++ _theme.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const theme = StyleSheet.create({
+  style1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  text1: {
+    color: '#1a1a1a',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+});
+
+export default theme;
